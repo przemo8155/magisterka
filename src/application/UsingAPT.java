874396eee028f426f8d967f6987fc5bc66deb3ed
przemo8155 <przemo8155@wp.pl>
2017-11-02 16:18:03 +0100

    Poprawki

--- src/application/UsingAPT.java
+++ src/application/UsingAPT.java
@@ -1,18 +1,36 @@
+
 package application;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+import com.itextpdf.kernel.pdf.WriterProperties;
 
 public class UsingAPT
 {
+
 	public void run() throws IOException
 	{
-		// Run a java app in a separate system process
-		Process proc = Runtime.getRuntime().exec("java -jar apt.jar");
-		// Then retreive the process output
-		InputStream in = proc.getInputStream();
-		InputStream err = proc.getErrorStream();
-		System.out.println(err);
-		System.out.println(in);
+		// ProcessBuilder pb = new ProcessBuilder("java", "-jar", "apt.jar");
+		// pb.directory(new File("TEMP"));
+		// Process p = pb.start();
+		try
+		{
+			ProcessBuilder builder = new ProcessBuilder("java -jar apt.jar");
+			builder.redirectErrorStream(true);
+			Process process = builder.start();
+			Scanner scanner = null;
+
+
+		} catch (IOException e)
+		{
+			e.fillInStackTrace();
+		}
 	}
 
 }
