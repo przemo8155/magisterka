4a62d55324bca3c24f62c2b3d7501fd585bfd9bb
przemo8155 <przemo8155@wp.pl>
2017-11-03 10:23:30 +0100

    Zmniejszanie liczby tokenow do 10

--- src/application/MainWindowController.java
+++ src/application/MainWindowController.java
@@ -818,11 +818,18 @@ public class MainWindowController
 						double positionX = c.getCenterX() - 20;
 						double positionY = c.getCenterY() - 20;
 						int i = checkBitmapToken(positionX, positionY);
-						if (i > 1)
+						if (i > 1 && i <= 10)
 						{
 							deleteBitmapToken(positionX, positionY);
 							setBitmapToken(c, i - 1);
 
+						}
+
+						else if(i > 10)
+						{
+							i-=1;
+							deleteTokenBiggerThanTen(positionX, positionY);
+							setBitmapToken(c, i - 1);
 						}
 
 						else
