e98ba8b29ec002b9a7bbb434abdefe68226657b4
przemo8155 <przemo8155@wp.pl>
2018-08-01 19:01:34 +0200

    Added tree

--- src/application/LTSController.java
+++ src/application/LTSController.java
@@ -9,6 +9,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
@@ -41,6 +42,16 @@ public class LTSController
 	ObservableList<String> statesDone = FXCollections.observableArrayList();
 	ObservableList<String> tempArc = FXCollections.observableArrayList();
 	ObservableList<String> tempLab = FXCollections.observableArrayList();
+
+	ObservableList<String> level1 = FXCollections.observableArrayList();
+	ObservableList<String> level2 = FXCollections.observableArrayList();
+	ObservableList<String> level3 = FXCollections.observableArrayList();
+	ObservableList<String> level4 = FXCollections.observableArrayList();
+	ObservableList<String> level5= FXCollections.observableArrayList();
+	ObservableList<String> level6 = FXCollections.observableArrayList();
+	ObservableList<String> level7 = FXCollections.observableArrayList();
+	ObservableList<String> level8 = FXCollections.observableArrayList();
+
 
 	private String filePath = "";
 
@@ -79,15 +90,128 @@ public class LTSController
 					s = s.substring(0, spaceIndex);
 				}
 
-				createCircle(width/2, 200, s);
 				initial = s;
 
-
+				level1.add(initial);
 			}
 		}
 
+		for(String s : arcsListFromFile)
+		{
+			String[] parts = s.split(" ");
+			for(String state : level1)
+			{
+				if(state.equals(parts[0]) && !level2.contains(parts[2]) && !level1.contains(parts[2]))
+				{
+					level2.add(parts[2]);
+				}
+			}
+
+			for(String state : level2)
+			{
+				if(state.equals(parts[0]) && !level3.contains(parts[2]) && !level2.contains(parts[2]) && !level1.contains(parts[2]))
+				{
+					level3.add(parts[2]);
+				}
+			}
+
+			for(String state : level3)
+			{
+				if(state.equals(parts[0]) && !level4.contains(parts[2]) && !level3.contains(parts[2]) && !level2.contains(parts[2]) && !level1.contains(parts[2]))
+				{
+					level4.add(parts[2]);
+				}
+			}
+
+			for(String state : level4)
+			{
+				if(state.equals(parts[0]) && !level5.contains(parts[2]) && !level4.contains(parts[2]) && !level3.contains(parts[2]) && !level2.contains(parts[2]) && !level1.contains(parts[2]))
+				{
+					level5.add(parts[2]);
+				}
+			}
+		}
+
+		for(String s : level1)
+		{
+			createCircle(width/2, 200, s);
+			System.out.println("level 1: " + s);
+		}
+
+		System.out.println();
+
+		for(String s : level2)
+		{
+			createCircle((level2.indexOf(s) + 1) * (width / (level2.size() + 1)), 300, s);
+			System.out.println("level 2: " + s);
+		}
+
+		System.out.println();
+
+		for(String s : level3)
+		{
+			createCircle((level3.indexOf(s) + 1) * (width / (level3.size() + 1)), 400, s);
+			System.out.println("level 3: " + s);
+		}
+
+		System.out.println();
+
+		for(String s : level4)
+		{
+			createCircle((level4.indexOf(s) + 1) * (width / (level4.size() + 1)), 500, s);
+			System.out.println("level 4: " + s);
+		}
+
+		System.out.println();
+
+		for(String s : level5)
+		{
+			createCircle((level5.indexOf(s) + 1) * (width / (level5.size() + 1)), 600, s);
+			System.out.println("level 5: " + s);
+		}
 
 
+		double levelY = 400;
+		for(Node n : mainPane.getChildren())
+		{
+			if(n.getLayoutY() > levelY) levelY = n.getLayoutY();
+		}
+
+		scrollMainPane.setContent(mainPane);
+		scrollMainPane.setMinHeight(levelY + 60);
+		scrollMainPane.setMaxHeight(levelY + 60);
+		mainPane.setMinHeight(levelY + 60);
+		mainPane.setMinHeight(levelY + 60);
+		this.closeButton.setLayoutY(levelY + 60);
+
+
+	}
+
+	public void drawTree(TreeNode<String> tree) {
+
+	}
+
+	public static TreeNode<String> getSet1() {
+		TreeNode<String> root = new TreeNode<String>("root");
+		{
+			TreeNode<String> node0 = root.addChild("node0");
+			TreeNode<String> node1 = root.addChild("node1");
+			TreeNode<String> node2 = root.addChild("node2");
+			{
+				TreeNode<String> node20 = node2.addChild("node20");
+				TreeNode<String> node21 = node2.addChild("node21");
+				{
+					TreeNode<String> node210 = node21.addChild("node210");
+					TreeNode<String> node211 = node21.addChild("node211");
+				}
+			}
+			TreeNode<String> node3 = root.addChild("node3");
+			{
+				TreeNode<String> node30 = node3.addChild("node30");
+			}
+		}
+
+		return root;
 	}
 
 
